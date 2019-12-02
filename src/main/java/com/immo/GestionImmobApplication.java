8a0728cbc1c5f565ce64aa9ae040b0d2043b60db
mory <mory.sangare@agrisoft-ci.com>
2019-12-02 17:35:39 +0000

    map

--- src/main/java/com/immo/GestionImmobApplication.java
+++ src/main/java/com/immo/GestionImmobApplication.java
@@ -1,51 +1,26 @@
 package com.immo;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class GestionImmobApplication {
 
-	/*private static final int HTTP_PORT = 80;
-	private static final int HTTPS_PORT = 443;
-	private static final String HTTP = "http";
-	private static final String USER_CONSTRAINT = "CONFIDENTIAL";
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint(USER_CONSTRAINT);
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("*//*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(redirectConnector());
-		return tomcat;
-	}
-
-	private Connector redirectConnector() {
-		Connector connector = new Connector(
-				TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme(HTTP);
-		connector.setPort(HTTP_PORT);
-		connector.setSecure(false);
-		connector.setRedirectPort(HTTPS_PORT);
-		return connector;
-	}*/
-
 	public static void main(String[] args) {
 		SpringApplication.run(GestionImmobApplication.class, args);
+		/*try {
+			openHomePages();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
 	}
 
+	/*private static void openHomePages() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8082/login");
+	}*/
 }
